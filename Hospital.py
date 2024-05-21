c9b9edee3f57bde8ae632fc4c5c70e54893fb324
Meuni <meuniupeka0917@gmail.com>
2024-05-21 09:16:45 +0530

    Hospital.py is modified with a new content

---
+++ Hospital.py
@@ -0,0 +1,38 @@
+# hospital.py
+
+from patient import Patient
+from doctor import Doctor
+from appointment import Appointment
+
+class Hospital:
+    def __init__(self):
+        self.patients = {}
+        self.doctors = {}
+        self.appointments = {}
+
+    def add_patient(self, patient):
+        self.patients[patient.person_id] = patient
+        print(f"Patient {patient.name} added.")
+
+    def add_doctor(self, doctor):
+        self.doctors[doctor.person_id] = doctor
+        print(f"Doctor {doctor.name} added.")
+
+    def schedule_appointment(self, appointment):
+        self.appointments[appointment.appointment_id] = appointment
+        print(f"Appointment {appointment.appointment_id} scheduled.")
+
+    def display_all_patients(self):
+        print("All Patients:")
+        for patient in self.patients.values():
+            patient.display_info()
+
+    def display_all_doctors(self):
+        print("All Doctors:")
+        for doctor in self.doctors.values():
+            doctor.display_info()
+
+    def display_all_appointments(self):
+        print("All Appointments:")
+        for appointment in self.appointments.values():
+            appointment.display_info()
