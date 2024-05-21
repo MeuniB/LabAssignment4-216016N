f1d561dde11a7e48ad6f747316aad6b5eab27848
Meuni <meuniupeka0917@gmail.com>
2024-05-21 09:29:11 +0530

    Doctor.py is added and modified

---
+++ Doctor.py
@@ -0,0 +1,12 @@
+# doctor.py
+
+from person import Person
+
+class Doctor(Person):
+    def __init__(self, person_id, name, age, specialty):
+        super().__init__(person_id, name, age)
+        self.specialty = specialty
+
+    def display_info(self):
+        super().display_info()
+        print(f"Specialty: {self.specialty}")
