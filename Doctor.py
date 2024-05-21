51c17041dbed64d90445e55570828c43bc8912bc
MeuniB <basnayakebmmu.21@uom.lk>
2024-05-21 11:43:27 +0530

    Updated Doctor.py

--- Doctor.py
+++ Doctor.py
@@ -1,5 +1,4 @@
-# doctor.py
-
+# Doctor.py
 from Person import Person
 
 class Doctor(Person):
@@ -10,3 +9,14 @@ class Doctor(Person):
     def display_info(self):
         super().display_info()
         print(f"Specialty: {self.specialty}")
+
+class Specialist(Doctor):
+    def __init__(self, person_id, name, age, specialty, subspecialty, certifications):
+        super().__init__(person_id, name, age, specialty)
+        self.subspecialty = subspecialty
+        self.certifications = certifications
+
+    def display_info(self):
+        super().display_info()
+        print(f"Subspecialty: {self.subspecialty}")
+        print(f"Certifications: {', '.join(self.certifications)}")
